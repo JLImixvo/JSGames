0fba470f3b698d936b4c779fa5308280af864bc0
SynnDev <alexifeltrinytb@gmail.com>
2022-05-04 11:38:10 +0200

    Ajout des commentaires, amélioration du code

--- js/function.js
+++ js/function.js
@@ -1,96 +1,91 @@
+// Function for get random number
+
 function getRandomNum(max) {
   return Math.floor(Math.random() * max + 1);
 }
 
-var th = ['', 'thousand', 'million', 'billion', 'trillion'];
-var dg = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-var tn = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-var tw = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+// Function for change number oon word
+var th = ["", "thousand", "million", "billion", "trillion"];
+var dg = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",];
+var tn = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",];
+var tw = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety",];
 
 function toWords(s) {
   s = s.toString();
-  s = s.replace(/[\, ]/g, '');
-  if (s != parseFloat(s)) return 'not a number';
-  var x = s.indexOf('.');
-  if (x == -1)
-    x = s.length;
-  if (x > 15)
-    return 'too big';
-  var n = s.split('');
-  var str = '';
+  s = s.replace(/[\, ]/g, "");
+  if (s != parseFloat(s)) return "not a number";
+  var x = s.indexOf(".");
+  if (x == -1) x = s.length;
+  if (x > 15) return "too big";
+  var n = s.split("");
+  var str = "";
   var sk = 0;
   for (var i = 0; i < x; i++) {
     if ((x - i) % 3 == 2) {
-      if (n[i] == '1') {
-        str += tn[Number(n[i + 1])] + ' ';
+      if (n[i] == "1") {
+        str += tn[Number(n[i + 1])] + " ";
         i++;
         sk = 1;
       } else if (n[i] != 0) {
-        str += tw[n[i] - 2] + ' ';
+        str += tw[n[i] - 2] + " ";
         sk = 1;
       }
-    } else if (n[i] != 0) { // 0235
-      str += dg[n[i]] + ' ';
-      if ((x - i) % 3 == 0) str += 'hundred ';
+    } else if (n[i] != 0) {
+      // 0235
+      str += dg[n[i]] + " ";
+      if ((x - i) % 3 == 0) str += "hundred ";
       sk = 1;
     }
     if ((x - i) % 3 == 1) {
-      if (sk)
-        str += th[(x - i - 1) / 3] + ' ';
+      if (sk) str += th[(x - i - 1) / 3] + " ";
       sk = 0;
     }
   }
 
   if (x != s.length) {
     var y = s.length;
-    str += 'point ';
-    for (var i = x + 1; i < y; i++)
-      str += dg[n[i]] + ' ';
+    str += "point ";
+    for (var i = x + 1; i < y; i++) str += dg[n[i]] + " ";
   }
-  return str.replace(/\s+/g, ' ');
-}
-
-function wait(ms) {
-  var start = new Date().getTime();
-  var end = start;
-  while (end < start + ms) {
-    end = new Date().getTime();
-  }
+  return str.replace(/\s+/g, " ");
 }
 
 function mode(theme) {
   if (theme == "white") {
-    colorMode.classList.remove("white")
-    colorMode.classList.add("dark")
+    // Change class for white mode
+    colorMode.classList.remove("white");
+    colorMode.classList.add("dark");
 
+    document.getElementById("containers").classList.add("bg-light");
+    document.getElementById("containers").classList.remove("bg-dark");
+    document.getElementById("sun").classList.add("text-dark");
+    document.getElementById("sun").classList.remove("text-light");
+    p1.classList.remove("text-light");
+    p2.classList.remove("text-light");
+    p1.classList.add("text-dark");
+    p2.classList.add("text-dark");
+    dice.classList.add("text-dark");
+    dice.classList.remove("text-light");
 
-    document.getElementById("containers").classList.add("bg-light")
-    document.getElementById("containers").classList.remove("bg-dark")
-    document.getElementById("sun").classList.add("text-dark")
-    document.getElementById("sun").classList.remove("text-light")
-    p1.classList.remove("text-light")
-    p2.classList.remove("text-light")
-    p1.classList.add("text-dark")
-    p2.classList.add("text-dark")
-    dice.classList.add("text-dark")
-    dice.classList.remove("text-light")
-
-    localStorage.setItem("theme", theme)
+    // Insert color theme in local storage
+    localStorage.setItem("theme", theme);
   } else {
-    colorMode.classList.add("white")
-    colorMode.classList.remove("dark")
+    // Change class for dark mode
+    colorMode.classList.add("white");
+    colorMode.classList.remove("dark");
 
-    document.getElementById("containers").classList.remove("bg-light")
-    document.getElementById("containers").classList.add("bg-dark")
-    document.getElementById("sun").classList.add("text-light")
-    document.getElementById("sun").classList.remove("text-dark")
-    p1.classList.add("text-light")
-    p2.classList.add("text-light")
-    p1.classList.remove("text-dark")
-    p2.classList.remove("text-dark")
-    dice.classList.remove("text-dark")
-    dice.classList.add("text-light")
+    document.getElementById("containers").classList.remove("bg-light");
+    document.getElementById("containers").classList.add("bg-dark");
+    document.getElementById("sun").classList.add("text-light");
+    document.getElementById("sun").classList.remove("text-dark");
+    p1.classList.add("text-light");
+    p2.classList.add("text-light");
+    p1.classList.remove("text-dark");
+    p2.classList.remove("text-dark");
+    dice.classList.remove("text-dark");
+    dice.classList.add("text-light");
 
-    localStorage.setItem("theme", theme)
+    // Insert color theme in local storage
+    localStorage.setItem("theme", theme);
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// initialize element id
 var total1 = document.getElementById("scoreTotal1");
 var total2 = document.getElementById("scoreTotal2");
 var currentScore1 = document.getElementById("currentScore1");
@@ -10,96 +11,122 @@ var dice = document.getElementById("dice");
 var roll = document.getElementById("roll");
 var hold = document.getElementById("hold");
 
-var newGames = document.getElementById("newGames")
+var newGames = document.getElementById("newGames");
 
-let currentPlayer = 1
+// Current player play
+let currentPlayer = 1;
 
+// Action for button "ROLL DICE"
 roll.addEventListener("click", function () {
+  // Get random number
   randomNum = getRandomNum(6);
+  // Get name for number generate
   let numName = toWords(randomNum);
 
+  // Dice face generate
   dice.innerHTML = '<i class="fa-regular fa-dice-' + numName + '"></i>';
 
-  // timer()
-
   if (currentPlayer == 1) {
-
     if (randomNum == 1) {
-      p1.classList.remove("activePlayer")
-      p2.classList.add("activePlayer")
-      currentPlayer = 2
-      currentScore1.innerHTML = 0
+      // If player 1 get number 1 then it's up to player 2 to play
+      p1.classList.remove("activePlayer");
+      p2.classList.add("activePlayer");
+      currentPlayer = 2;
+      currentScore1.innerHTML = 0;
+    } else {
+      // If the player 1 has a number other than 1 then the content is added in CurrentScore
+      let number = parseInt(currentScore1.innerHTML);
+      currentScore1.innerHTML = number + randomNum;
     }
-
-    let number = parseInt(currentScore1.innerHTML)
-    currentScore1.innerHTML = number + randomNum
   } else {
-
     if (randomNum == 1) {
-      p2.classList.remove("activePlayer")
-      p1.classList.add("activePlayer")
-      currentPlayer = 1
-      currentScore2.innerHTML = 0
+      // If player 2 get number 1 then it's up to player 1 to play
+      p2.classList.remove("activePlayer");
+      p1.classList.add("activePlayer");
+      currentPlayer = 1;
+      currentScore2.innerHTML = 0;
+    } else {
+      // If the player 2 has a number other than 1 then the content is added in CurrentScore
+      let number = parseInt(currentScore2.innerHTML);
+      currentScore2.innerHTML = number + randomNum;
     }
-
-    let number = parseInt(currentScore2.innerHTML)
-    currentScore2.innerHTML = number + randomNum
   }
 });
 
+// Action for button "HOLD"
 hold.addEventListener("click", function () {
   if (currentPlayer == 1) {
-    let number = parseInt(currentScore1.innerHTML)
-    let total = parseInt(total1.innerHTML)
+    let number = parseInt(currentScore1.innerHTML);
+    let total = parseInt(total1.innerHTML);
 
-    total1.innerHTML = total + number
+    total1.innerHTML = total + number;
 
-    currentScore1.innerHTML = 0
-    currentPlayer = 2
-    p1.classList.remove("activePlayer")
-    p2.classList.add("activePlayer")
-
+    // Check if player 1 take 100 points and attach a message for win
+    if (total1.innerHTML >= 100) {
+      currentScore1.innerHTML = "Congratulations you won";
+      roll.disabled = true;
+      hold.disabled = true;
+    } else {
+      currentScore1.innerHTML = 0;
+      currentPlayer = 2;
+      p1.classList.remove("activePlayer");
+      p2.classList.add("activePlayer");
+    }
   } else {
-    let number = parseInt(currentScore2.innerHTML)
-    let total = parseInt(total2.innerHTML)
+    // Check if player 2 take 100 points and attach a message for win
+    let number = parseInt(currentScore2.innerHTML);
+    let total = parseInt(total2.innerHTML);
 
-    total2.innerHTML = total + number
-
-    currentScore2.innerHTML = 0
-    currentPlayer = 1
-    p2.classList.remove("activePlayer")
-    p1.classList.add("activePlayer")
+    total2.innerHTML = total + number;
+    if (total2.innerHTML >= 100) {
+      currentScore2.innerHTML = "Congratulations you won";
+      roll.disabled = true;
+      hold.disabled = true;
+    } else {
+      currentScore2.innerHTML = 0;
+      currentPlayer = 1;
+      p2.classList.remove("activePlayer");
+      p1.classList.add("activePlayer");
+    }
   }
 });
 
+// Action for button "NEW GAMES"
 newGames.addEventListener("click", function () {
-  p2.classList.remove("activePlayer")
-  p1.classList.add("activePlayer")
-  currentPlayer = 1
+  // Reset score
+  p2.classList.remove("activePlayer");
+  p1.classList.add("activePlayer");
+  currentPlayer = 1;
 
-  currentScore1.innerHTML = 0
-  total1.innerHTML = 0
-  currentScore2.innerHTML = 0
-  total2.innerHTML = 0
-})
+  currentScore1.innerHTML = 0;
+  total1.innerHTML = 0;
+  currentScore2.innerHTML = 0;
+  total2.innerHTML = 0;
+  roll.disabled = false;
+  hold.disabled = false;
+});
 
-var colorMode = document.getElementById("colorMode")
-let color
+// Initalize theme color
+var colorMode = document.getElementById("colorMode");
+let color;
 
+// Get last theme mode use on local storage
 let theme = localStorage.getItem("theme");
 
+// Get if theme color it's initialize or no
 if (theme != null) {
-  color = theme
-  mode(theme)
+  color = theme;
+  mode(theme);
 }
 
+// Action color mode
 colorMode.addEventListener("click", function () {
-  console.log(color)
+  console.log(color);
   if (color == "black") {
-    mode("white")
-    color = "white"
+    mode("white");
+    color = "white";
   } else {
-    mode("black")
-    color = "black"
+    mode("black");
+    color = "black";
   }
-})
+});
